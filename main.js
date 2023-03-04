51771aa54cc56dbc8aad0ec011c97f7a28302357
Samy572 <samy.chabbi@gmx.fr>
2023-03-04 22:25:15 +0100

    fin de la mise en page version mobile

--- main.js
+++ main.js
@@ -4,15 +4,24 @@ import './slider';
 const menu = document.querySelector('.menu');
 const hide = document.querySelector('.hide');
 const body = document.querySelector('body');
+const closeBtn = document.getElementById('close');
 let toggle = false;
 
 menu.addEventListener('click', function () {
 	toggle = !toggle;
 	if (toggle === true) {
 		hide.style.display = 'flex';
-    body.style.overflow = 'hidden'; 
+		body.style.overflow = 'hidden';
 	} else if (toggle === false) {
 		hide.style.display = 'none';
-    body.style.overflow = 'auto'; 
+		body.style.overflow = 'auto';
+	}
+});
+
+closeBtn.addEventListener('click', function () {
+	if (toggle === true) {
+		toggle = !toggle;
+		hide.style.display = 'none';
+		body.style.overflow = 'auto';
 	}
 });
